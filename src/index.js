af41ea75022b42823f0eb0f46131091a441a1d36
Nastassia Zharnasek <Nastassia_Zharnasek@epam.com>
2020-10-12 19:10:44 +0300

    init: start towelsort task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if (matrix === undefined || matrix.length === 0) return [];
+
+    var result = [];
+    matrix.forEach((row, i) => {
+        i % 2 == 0 ? row.sort((a, b) => a - b) : row.sort((a, b) => b - a);
+        result = result.concat(row);
+    });
+    return result;
+};
